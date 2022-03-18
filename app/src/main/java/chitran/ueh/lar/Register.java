fa92f4ec90c6544f6395946f67df17d7f55629da
chitran1903 <chitran.31191024357@st.ueh.edu.vn>
2022-03-18 23:11:17 +0700

    first commit

---
+++ app/src/main/java/chitran/ueh/lar/Register.java
@@ -0,0 +1,13 @@
+package chitran.ueh.lar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
